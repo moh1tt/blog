5c7df26b8c4bfaae8327221f713dca590abd43c4
Mohit Appari <mohitt.appari@gmail.com>
2024-02-18 22:46:10 -0500

    comment activity.js

--- pages/activity.js
+++ pages/activity.js
@@ -1,21 +1,21 @@
-import { MDXLayoutRenderer } from '@/components/MDXComponents'
-import { getFileBySlug } from '@/lib/mdx'
+// import { MDXLayoutRenderer } from '@/components/MDXComponents'
+// import { getFileBySlug } from '@/lib/mdx'
 
-const DEFAULT_LAYOUT = 'ActivityLayout'
+// const DEFAULT_LAYOUT = 'ActivityLayout'
 
-export async function getStaticProps() {
-  const activityDetails = await getFileBySlug('activity', ['default'])
-  return { props: { activityDetails } }
-}
+// export async function getStaticProps() {
+//   const activityDetails = await getFileBySlug('activity', ['default'])
+//   return { props: { activityDetails } }
+// }
 
-export default function Activity({ activityDetails }) {
-  const { mdxSource, frontMatter } = activityDetails
+// export default function Activity({ activityDetails }) {
+//   const { mdxSource, frontMatter } = activityDetails
 
-  return (
-    <MDXLayoutRenderer
-      layout={frontMatter.layout || DEFAULT_LAYOUT}
-      mdxSource={mdxSource}
-      frontMatter={frontMatter}
-    />
-  )
-}
+//   return (
+//     <MDXLayoutRenderer
+//       layout={frontMatter.layout || DEFAULT_LAYOUT}
+//       mdxSource={mdxSource}
+//       frontMatter={frontMatter}
+//     />
+//   )
+// }
